4874bf1b0d9540bfba185f24af1b138600ac2943
meersausteven <36692921+meersausteven@users.noreply.github.com>
2019-11-04 08:43:22 +0100

    Update save_game.js

--- js/save_game.js
+++ js/save_game.js
@@ -5,7 +5,7 @@ var saveprogress_interval = setInterval(saveProgress, 120000);
 
 function saveProgress() {
 	playtime = Date.now() - starttime;
-	var save = totalhamstercount +
+	var save = window.btoa(totalhamstercount +
 		"-" + hamstercount +
 		"-" + cagedhamsterclicks +
 		"-" + rarehamsterclicks +
@@ -18,7 +18,7 @@ function saveProgress() {
 		"-" + hamstersperclick +
 		"-" + starttime +
 		"-" + playtime
-	;
+	);
 	
 	console.log(save);
 	localStorage.setItem("savegame", save);
