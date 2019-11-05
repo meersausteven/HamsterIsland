ec5025ffa09dc73e4f1d6db92fddd5ed2e93aa00
meersausteven <36692921+meersausteven@users.noreply.github.com>
2019-11-05 12:49:18 +0100

    Fixed function that increases hamstercount

--- js/general.js
+++ js/general.js
@@ -38,7 +38,7 @@ function increaseHamsters(amount) {
 		}
 		for (i = 0; i < 10; i++) {
 			setTimeout(increaseCounter(), 100);
-			
+			// only run every 0.1 sec
 			function increaseCounter() {
 				if (i != 10) {
 					hamstercount += Math.floor(amount / 10);
