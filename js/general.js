8d6936ef7efbb332eacaced99b23c105b40a8339
meersausteven <36692921+meersausteven@users.noreply.github.com>
2019-12-12 11:05:59 +0100

    Update general.js

--- js/general.js
+++ js/general.js
@@ -39,7 +39,7 @@ function increaseHamsters(amount) {
 			totalhamstercount += amount;
 		}
 		for (i = 0; i <= 10; i++) {
-			setTimeout(increaseCounter(), 1000);
+			window.setTimeout(increaseCounter(), 100);
 			// only run every 0.1 sec
 			function increaseCounter() {
 				console.log(i);
