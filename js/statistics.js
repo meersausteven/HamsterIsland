612e7ea199688cc2dcc9c4311a61ae34be260e06
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-01-03 13:20:40 +0100

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -4,7 +4,8 @@
 var updatestats_interval = setInterval(updateStats, 10000);
 
 function updateStats() {
-	var time = starttime - new Date().getTime();
+	var time = starttime - Date.now();
+	console.log("start: " + starttime + " | now: " + Date.now();
 	var playedseconds  = new Date(time).getSeconds();
 	var playedminutes  = new Date(time).getMinutes();
 	var playedhours  = new Date(time).getHours();
