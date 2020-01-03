cdbe622257c07dd1ff4601fc17b92fcf2eed9118
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-01-03 14:23:04 +0100

    Update rarehamster.js

--- js/rarehamster.js
+++ js/rarehamster.js
@@ -40,7 +40,8 @@ function rareHamsterBoostDuration(rarehamster) {
 	document.body.appendChild(countdown);
 	
 	var countdownDuration = rarehamster.duration;
-	var decreaseDisplay = setInterval(function() {console.log((countdownDuration / rarehamster.duration) * 100);
+	var decreaseDisplay = setInterval(function() {
+		console.log("prozent über: " + (countdownDuration / rarehamster.duration) * 100);
 		document.getElementById("rareHamsterBoostCountdownOverlay").style.cssText = "width:" + ((countdownDuration / rarehamster.duration) * 100) + "%;";
 		countdownDuration -= 100;
 	}, 100);
