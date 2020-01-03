a7ef7ca75680a824e251440c18613f66a13627c5
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-01-03 13:16:20 +0100

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -5,9 +5,9 @@ var updatestats_interval = setInterval(updateStats, 10000);
 var time = starttime - new Date().getTime();
 
 function updateStats() {
-	var playedseconds  = Date(time).getSeconds();
-	var playedminutes  = Date(time).getMinutes();
-	var playedhours  = Date(time).getHours();
+	var playedseconds  = new Date(time).getSeconds();
+	var playedminutes  = new Date(time).getMinutes();
+	var playedhours  = new Date(time).getHours();
 	var totalplaytime = playedhours + ((playedhours == 1) ? " hour " : " hours ") + " hours " + playedminutes + ((playedminutes == 1) ? " minute " : " minutes ") + playedseconds + ((playedseconds == 1) ? " second " : " seconds");
 
 	document.getElementById("stats_hamsters").innerHTML = convertToReadableNumber(hamstercount);
