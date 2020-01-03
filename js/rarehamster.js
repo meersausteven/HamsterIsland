f241db3f5d708500cf0fb1450941345ffe989b48
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-01-03 09:33:30 +0100

    Added new function
    - Name: "rareHamsterBoostDuration"
    - Displays a small div that indicates the rest duration of a rare hamster boost
    - depletes over time

--- js/rarehamster.js
+++ js/rarehamster.js
@@ -12,12 +12,45 @@ function spawnRareHamster(type) {
 
 // boost the hps if a rare hamsters gives that effect
 
-function rareHamsterBoost(multiplier, duration) {
-	hamsterspersecondboost = multiplier;
+function rareHamsterBoost(that) {
+	hamsterspersecondboost = that.multiplier;
 	updateHPS();
-	var cancelboosttimeout = setTimeout(cancelBoost, duration);
+	var cancelboosttimeout = setTimeout(cancelBoost, that.duration);
 	function cancelBoost() {
 		hamsterspersecondboost = 1;
 		updateHPS();
+	}
+}
+
+function rareHamsterBoostDuration(that) {
+	let countdown = document.getElementById("rareHamsterBoostCountdown");
+	if (!countdown) {
+		countdown = document.createElement("div");
+		countdown.setAttribute("id", "rareHamsterBoostCountdown");
+	}
+	countdown.classname = "background-preset";
+	countdown.cssText = "background-image: url(rare_hamster_countdown_" + that.rarehamstertype + ".png);";
+	
+	let countdownOverlay = document.getElementById("rareHamsterBoostCountdownOverlay");
+	if (!element) {
+		countdownOverlay = document.createElement("div");
+		countdownOverlay.setAttribute("id", "rareHamsterBoostCountdownOverlay");
+	}
+	countdownOverlay.className = "countdown-overlay";
+	countdownOverlay.cssText = "background-image: url(rare_hamster_countdown_" + that.rarehamstertype + ".png);";
+	
+	countdown.appendChild(countdownOverlay);
+	document.body.appendChild(countdown);
+	let countdownDuration = that.duration;
+	setInterval(function() {
+		document.getElementById("rareHamsterBoostCountdownOverlay").cssText = "width:" + ((countdownDuration / that.duration) * 100) + "%;";
+		countdownDuration -= 100;
+	}, 100);
+	setTimeout(function() {this.delete()}.bind(this), that.boostDuration);
+	}
+	delete() {
+		if (document.getElementById("rareHamsterBoostCountdown") != null) {
+			document.body.removeChild(this.element);
+		}
 	}
 }
