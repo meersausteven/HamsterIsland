9dbd7db8e19685b4e8c71d736c239c2ec9d01b24
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-01-24 10:25:28 +0100

    Update upgrade_shop.js

--- js/upgrade_shop.js
+++ js/upgrade_shop.js
@@ -266,7 +266,7 @@ function addNewUpgradesToShop() {
 				}
 				// move info box with mouse but keep it from moving outside of the screen
 				//var correctLeft = e.clientX - (upgradebox.clientWidth / 2);
-				var correctLeft = (e.clientX - (upgradebox.clientWidth / 2)) - ((e.clientX - upgradeposition.left - (upgradebox.clientWidth / 2)) * 0,5);
+				var correctLeft = (e.clientX - ((upgradebox.clientWidth - 10) / 2)) - ((e.clientX - upgradeposition.left - (upgradebox.clientWidth / 2)) / 2);
 				if (correctLeft < 10) {
 					correctLeft = 10;
 				} else if (correctLeft > window.innerWidth - upgradebox.clientWidth - 10) {
