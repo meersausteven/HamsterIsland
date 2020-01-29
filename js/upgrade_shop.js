ebf6987de940c4be117ffa590629e3218446125f
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-01-29 08:07:15 +0100

    Update upgrade_shop.js

--- js/upgrade_shop.js
+++ js/upgrade_shop.js
@@ -133,7 +133,7 @@ var allUpgrades = cageupgrades.concat(foodupgrades, drinkupgrades, islandupgrade
 function addNewUpgradesToShop() {
   allUpgrades.forEach(function(upgrade) {
 	if ((upgrade.price <= totalhamstercount) && (!boughtUpgrades.includes(upgrade.id)) && (upgrade.displayed === false) ) {
-		if ( (upgrade.tag != "world") && (typeof(upgrade.world) != 'undefined') ) {
+		if ( (typeof upgrade.world !== 'undefined') && (upgrade.tag != "world") ) {
 			// only display upgrades that are one level higher than the current level of the upgrade type
 			if ( (upgrade.tag == "cage") && (cagelevel != upgrade.level - 1) && (upgrade.world == worldlevel) ) {
 				return;
