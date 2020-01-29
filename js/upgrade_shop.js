64e5c549d5caf750dfb6666b4ce42c2aabb199dc
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-01-29 08:49:15 +0100

    Fixed the shop

--- js/upgrade_shop.js
+++ js/upgrade_shop.js
@@ -145,12 +145,14 @@ function addNewUpgradesToShop() {
 					return;
 				} else if ( (upgrade.tag == "hamster") && (upgrade.level != hamsterlevel + 1) || (upgrade.world != worldlevel) ) {
 					return;
-				} console.log(upgrade);
+				}
 			} else if ( (upgrade.tag == "world") && (upgrade.level == worldlevel + 1) ) {
 				// only display world upgrades if all upgrades of the current world have been purchased
 				if ( (islandlevel != 5) || (drinklevel != 8) || (foodlevel != 8) || (cagelevel != 5) || (hamsterlevel != 3) ) {
 					return;
 				}
+			} else {
+				return;
 			}
 			upgrade.displayed = true;
 			var shopitem = new ShopItem(upgrade);
